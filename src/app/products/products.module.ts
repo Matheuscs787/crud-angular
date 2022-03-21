3dd8b93b61309ed591cf83a95e4a309f047205f6
Matheus787 <matheus787@hotmail.com>
2022-03-20 22:43:30 -0300

    Aula 7

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
@@ -13,7 +13,7 @@ import { ProductsComponent } from './products/products.component';
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class ProductsModule { }
